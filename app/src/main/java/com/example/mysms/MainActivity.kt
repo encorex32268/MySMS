2d001caf862bad71908828a1a1b4d4d82d61f9c5
ChenLiHan <encorex32268@gmail.com>
2021-04-05 10:53:35 +0900

    call phone done

--- app/src/main/java/com/example/mysms/MainActivity.kt
+++ app/src/main/java/com/example/mysms/MainActivity.kt
@@ -2,16 +2,19 @@ package com.example.mysms
 
 import android.Manifest
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.telephony.SmsManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import java.lang.reflect.Method
+import com.example.mysms.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,62 +22,70 @@ class MainActivity : AppCompatActivity() {
 
     private val MY_PERMISSIONS_REQUEST_SEND = 105
     private val TAG = MainActivity::class.java.simpleName
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
         checkPemission()
-        val count = findViewById<EditText>(R.id.countEditText)
-        val button  = findViewById<Button>(R.id.sendButton)
 
-        button.setOnClickListener {
-//            var sendCount = count.text.toString().toInt()
-//            for (i in 1..sendCount) {
-//                sendMessage()
-//            }
-
-            callPhoneNumber()
-
-
+        binding.apply {
+            phoneEditTextView.setText("08071760822")
+            sendButton.setOnClickListener {
+//                callPhone()
+                getPhone()
+            }
         }
 
     }
 
-    private fun callPhoneNumber() {
-        val telephonyManager = applicationContext.getSystemService(Context.TELEPHONY_SERVICE)
-        val clazz = Class.forName(telephonyManager.javaClass.name)
-        val method = clazz.getDeclaredMethod("getITelephony")
-        method.isAccessible = true
-        val telephonyService = method.invoke(telephonyManager) as ITelephony
-        telephonyService.endCall()
+    private fun ActivityMainBinding.getPhone() {
+
+
     }
 
-    private fun checkPemission(){
+    private fun ActivityMainBinding.callPhone() {
+        val packageManager = packageManager
+        val phoneNumber = phoneEditTextView.text.toString()
+        if (packageManager.hasSystemFeature(PackageManager.FEATURE_TELEPHONY)) {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:$phoneNumber")
+            startActivity(intent)
+        }
+    }
+
+
+    private fun checkPemission() {
 
         val sms_permission = ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)
         val callphone_permission = ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)
-        val permissions = arrayOf(Manifest.permission.SEND_SMS,Manifest.permission.CALL_PHONE)
-        val sms_permissionCheck  =  sms_permission != PackageManager.PERMISSION_GRANTED
-        val call_permissionCheck  = callphone_permission != PackageManager.PERMISSION_GRANTED
+        val permissions = arrayOf(Manifest.permission.SEND_SMS, Manifest.permission.CALL_PHONE)
+        val sms_permissionCheck = sms_permission != PackageManager.PERMISSION_GRANTED
+        val call_permissionCheck = callphone_permission != PackageManager.PERMISSION_GRANTED
 
-        if (sms_permissionCheck || call_permissionCheck){
+        if (sms_permissionCheck || call_permissionCheck) {
             ActivityCompat.requestPermissions(this@MainActivity, permissions,
-            MY_PERMISSIONS_REQUEST_SEND)
+                    MY_PERMISSIONS_REQUEST_SEND)
         }
-
 
 
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == MY_PERMISSIONS_REQUEST_SEND){
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+        if (requestCode == MY_PERMISSIONS_REQUEST_SEND) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Toast.makeText(this@MainActivity,
                         "SMS Call Permission Granted",
                         Toast.LENGTH_SHORT)
                         .show()
-            }else{
+            } else {
                 Toast.makeText(this@MainActivity,
                         "SMS Permission Denied",
                         Toast.LENGTH_SHORT)
@@ -82,9 +93,10 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-    private fun sendMessage(){
-        val  smsManager = SmsManager.getDefault()
-        smsManager.sendTextMessage("080-7176-0822",null,"test 123 ",null,null)
+
+    private fun sendMessage() {
+        val smsManager = SmsManager.getDefault()
+        smsManager.sendTextMessage("080-7176-0822", null, "test 123 ", null, null)
         Toast.makeText(this@MainActivity, "SMS sent.",
                 Toast.LENGTH_LONG).show();
     }
