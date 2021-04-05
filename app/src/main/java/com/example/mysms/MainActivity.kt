d6da4b0a970359630f513ecc4c139a56609cb571
ChenLiHan <encorex32268@gmail.com>
2021-04-05 10:18:29 +0900

    Temp commit

--- app/src/main/java/com/example/mysms/MainActivity.kt
+++ app/src/main/java/com/example/mysms/MainActivity.kt
@@ -1,23 +1,23 @@
 package com.example.mysms
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.telephony.SmsManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-
+import java.lang.reflect.Method
 
 
 class MainActivity : AppCompatActivity() {
 
 
-    private val MY_PERMISSIONS_REQUEST_SEND_SMS = 105
+    private val MY_PERMISSIONS_REQUEST_SEND = 105
     private val TAG = MainActivity::class.java.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,30 +30,48 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener {
 //            var sendCount = count.text.toString().toInt()
 //            for (i in 1..sendCount) {
-                sendMessage()
+//                sendMessage()
 //            }
+
+            callPhoneNumber()
 
 
         }
 
     }
+
+    private fun callPhoneNumber() {
+        val telephonyManager = applicationContext.getSystemService(Context.TELEPHONY_SERVICE)
+        val clazz = Class.forName(telephonyManager.javaClass.name)
+        val method = clazz.getDeclaredMethod("getITelephony")
+        method.isAccessible = true
+        val telephonyService = method.invoke(telephonyManager) as ITelephony
+        telephonyService.endCall()
+    }
+
     private fun checkPemission(){
 
         val sms_permission = ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)
-        val permissions = arrayOf(Manifest.permission.SEND_SMS)
-        if (sms_permission != PackageManager.PERMISSION_GRANTED){
+        val callphone_permission = ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)
+        val permissions = arrayOf(Manifest.permission.SEND_SMS,Manifest.permission.CALL_PHONE)
+        val sms_permissionCheck  =  sms_permission != PackageManager.PERMISSION_GRANTED
+        val call_permissionCheck  = callphone_permission != PackageManager.PERMISSION_GRANTED
+
+        if (sms_permissionCheck || call_permissionCheck){
             ActivityCompat.requestPermissions(this@MainActivity, permissions,
-            MY_PERMISSIONS_REQUEST_SEND_SMS)
+            MY_PERMISSIONS_REQUEST_SEND)
         }
+
+
 
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == MY_PERMISSIONS_REQUEST_SEND_SMS){
+        if (requestCode == MY_PERMISSIONS_REQUEST_SEND){
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
                 Toast.makeText(this@MainActivity,
-                        "SMS Permission Granted",
+                        "SMS Call Permission Granted",
                         Toast.LENGTH_SHORT)
                         .show()
             }else{
@@ -72,4 +90,10 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+}
+
+internal interface ITelephony {
+    fun endCall(): Boolean
+    fun answerRingingCall()
+    fun silenceRinger()
 }
